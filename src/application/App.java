50d6bd875cb5e28c2e9864739e68139e82444854
Gabriel Melo <gabrielmelodev321@gmail.com>
2024-03-05 18:25:33 -0300

    Chess layer and printing the board

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,12 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
         
     }
 }
