66b13378e57249bfbcee924e02ab04e4e2765aac
Gabriel Melo <gabrielmelodev321@gmail.com>
2024-04-13 22:40:13 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public ChessPosition getChessPosition(){
+        return ChessPosition.fromPosition(position);
+    }
+
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece piece = (ChessPiece) getBoard().piece(position);
         return piece != null && piece.getColor() != color;
